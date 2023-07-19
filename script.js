d19ebf88599fb8fb75b13929ee71a6f9ecc985d0
Alexander <akoceryzhkin@gmail.com>
2023-07-19 15:47:41 +0300

    first-commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let cloudButton = document.querySelectorAll('.cloud__button');
+let cloudButtonClose = document.querySelectorAll('.cloud__button-close');
+
+cloudButton.forEach(el => {
+    el.addEventListener('click', (e) => {
+        let parent = e.target.parentElement
+        parent.classList.add('cloud__rotate');
+        parent.children[0].classList.add('hidden')
+        parent.children[2].classList.add('hidden')
+        setTimeout(()=>{
+            parent.children[1].classList.remove('hidden')
+            parent.children[3].classList.remove('hidden')
+            parent.children[4].classList.remove('hidden')
+        }, 900)
+    })
+})
+
+cloudButtonClose.forEach(el => {
+    el.addEventListener('click', (e) => {
+        let parent = e.target.parentElement
+        parent.classList.remove('cloud__rotate')
+        parent.children[1].classList.add('hidden')
+        parent.children[3].classList.add('hidden')
+        parent.children[4].classList.add('hidden')
+        setTimeout(()=>{
+            parent.children[0].classList.remove('hidden')
+            parent.children[2].classList.remove('hidden')
+        }, 100)
+    })
+})
+
+
+
+
