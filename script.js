89984ac0b74e16f51f96234b3f781c9ceb2082a2
Nikita <nikitostokarev22@gmail.com>
2023-07-24 23:57:21 +0300

    text  block, fix some styles

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ modalButtonClose.addEventListener('click', () => {
 
 // Обработчик событий для кнопки "Вверх"
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 800){
+    if (window.scrollY > 600){
         btnUp.classList.remove('unvisible')
         header.classList.add('unvisible')
         btnUp.classList.add('visible')
